7fa5cc421a8c1a09f1253d7718270df6c8b7e242
Stanleylab97 <stanleylab97@github.com>
2021-05-03 17:11:13 +0100

    Init

--- android/app/src/main/kotlin/com/example/fr_parel/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fr_parel/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.fr_parel
+package com.stanleylab97.fr_parel
 
 import io.flutter.embedding.android.FlutterActivity
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:fr_parel/main.dart';
+import 'package:fr_parel/releve.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
